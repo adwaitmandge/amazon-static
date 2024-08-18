d081d8bd668860a3fb30cd25f76b5590cd292edb
Firefist01 <adwaitmandge@gmail.com>
2024-08-18 15:14:26 +0530

    Added firebase config

--- firebase.js
+++ firebase.js
@@ -4,14 +4,14 @@
 // // https://firebase.google.com/docs/web/setup#available-libraries
 
 // // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCvMeXEvpG40iUwaBqEc5e25dqBmGTfRUA",
-//   authDomain: "fir-c44fa.firebaseapp.com",
-//   projectId: "fir-c44fa",
-//   storageBucket: "fir-c44fa.appspot.com",
-//   messagingSenderId: "985711831746",
-//   appId: "1:985711831746:web:ba5cad0383a3b8a898a95b",
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyCvMeXEvpG40iUwaBqEc5e25dqBmGTfRUA",
+  authDomain: "fir-c44fa.firebaseapp.com",
+  projectId: "fir-c44fa",
+  storageBucket: "fir-c44fa.appspot.com",
+  messagingSenderId: "985711831746",
+  appId: "1:985711831746:web:ba5cad0383a3b8a898a95b",
+};
 
 // // Initialize Firebase
 const app = initializeApp(firebaseConfig);
